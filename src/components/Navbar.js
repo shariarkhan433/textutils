c0a553a10050ba66d83dbb6251ff3eef77557584
shariar khan <kshariare@outlook.com>
2024-05-30 04:54:46 +0600

    some error regarding dark mode message is solved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,7 @@ export default function Navbar(props) {
       </form> */}
             <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
               <input className="form-check-input" onClick={props.togglemode} type="checkbox" id="flexSwitchCheckDefault" />
-              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Switch Dark mode</label>
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Switch {props.mode} mode</label>
             </div>
           </div>
         </div>
